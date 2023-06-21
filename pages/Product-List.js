9842b9d7b17eef6249b920b937c613eba5a46775
shanewmurphy <shn.murphy@gmail.com>
2023-06-21 11:54:39 +0100

    Added Product List page

--- pages/Product-List.js
+++ pages/Product-List.js
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import Productlist from "@/Data/Productlist";
+
 export default function ProductList() {
   return (
     <div>
-      <div>AA</div>
+      <div>
+        <div></div>
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-11/12 mx-auto lg:mt-16 md:mt-8 sm:mt-6 gap-4">
+            {Productlist.map((product) => (
+              <div key={product.id} className="mt-6">
+                <Image
+                  src={product.productImgUrl}
+                  width={500}
+                  height={500}
+                  alt={product.productName}
+                />
+                <h2 className="text-center lg:font-medium sm:font-semibold text-text lg:text-lg md:text-lg sm:text-xs mt-2 antialiased">
+                  {product.productName}
+                </h2>
+                <p className="text-center text-text lg:font-medium sm:font-semibold lg:text-sm md:text-sm sm:text-xs">
+                  {product.Set}
+                </p>
+                <p className="text-center text-text lg:font-medium sm:font-normal lg:text-sm md:text-sm sm:text-xs">
+                  Price: €{product.price}
+                </p>
+                {/* <a href={product.PageLinkUrl}>View Details</a> */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
