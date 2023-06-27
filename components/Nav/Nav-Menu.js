bbdf00157034caed61c060cb019e41bfc2c7af78
shanewmurphy <shn.murphy@gmail.com>
2023-06-27 14:04:38 +0100

    Menu Mobile Links

--- components/Nav/Nav-Menu.js
+++ components/Nav/Nav-Menu.js
@@ -53,7 +53,7 @@ export default function NavMobile() {
                       <div className="absolute right-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
                         <button
                           type="button"
-                          className="rounded-md text-gray-300 hover:text-text"
+                          className="rounded-md text-button hover:text-text"
                           onClick={() => setOpen(false)}
                         >
                           <span className="sr-only">Close panel</span>
@@ -63,12 +63,78 @@ export default function NavMobile() {
                     </Transition.Child>
                     <div className="flex h-full flex-col overflow-y-scroll bg-background py-6 shadow-xl">
                       <div className="px-4 sm:px-6">
-                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                        {/* <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
                           Panel title
-                        </Dialog.Title>
+                        </Dialog.Title> */}
                       </div>
                       <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                        {/* Your content */}
+                        <div className="border-b-2 border-sand/40">
+                          <h4 className="font-semibold text-xl text-text antialiased">
+                            Shop
+                          </h4>
+                          <ul className="ml-2 mt-2 font-medium text-base text-text antialiased">
+                            <li>
+                              <a
+                                href="/Dinnerware-Collection"
+                                className="hover:text-button"
+                              >
+                                Dinnerware Sets
+                              </a>
+                            </li>
+                            <li className="mt-2">
+                              <a
+                                href="/Plates-Collection"
+                                className="hover:text-button"
+                              >
+                                Big Plates
+                              </a>
+                            </li>
+                            <li className="mt-2">
+                              <a
+                                href="/Bowels-Collection"
+                                className="hover:text-button"
+                              >
+                                Bowels
+                              </a>
+                            </li>
+                            <li className="mt-2 mb-4">
+                              <a
+                                href="/Cups-Collection"
+                                className="hover:text-button"
+                              >
+                                Cups
+                              </a>
+                            </li>
+                            <div className="divide-y"></div>
+                          </ul>
+                        </div>
+                        <div>
+                          <h4 className="font-semibold text-xl text-text mt-4 antialiased">
+                            About
+                          </h4>
+                          <ul className="ml-2 mt-2 font-medium text-base text-text antialiased">
+                            <li>
+                              <a href="/" className="hover:text-button">
+                                Our Story
+                              </a>
+                            </li>
+                            <li className="mt-2">
+                              <a href="/" className="hover:text-button">
+                                Sustainability
+                              </a>
+                            </li>
+                            <li className="mt-2">
+                              <a href="/" className="hover:text-button">
+                                FAQ
+                              </a>
+                            </li>
+                            <li className="mt-2">
+                              <a href="/" className="hover:text-button">
+                                Contact Us
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </Dialog.Panel>
